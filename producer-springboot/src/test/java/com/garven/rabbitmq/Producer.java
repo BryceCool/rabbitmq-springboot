7a1ccad55037e401e5e96d52ea217121b2e1e9b2
garven <425991867@qq.com>
2021-08-30 10:18:39 +0800

    producer -> exchange的confirm 机制

--- producer-springboot/src/test/java/com/garven/rabbitmq/Producer.java
+++ producer-springboot/src/test/java/com/garven/rabbitmq/Producer.java
@@ -3,6 +3,7 @@ package com.garven.rabbitmq;
 import com.garven.rabbitmq.config.RabbitMqConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +22,26 @@ public class Producer {
 
     @Test
     public void testSend() {
-        template.convertAndSend(RabbitMqConfig.EXCHANGE_NAME, "boot.start", "Hello Rabbit!");
+
+        template.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+
+            /**
+             * @param correlationData 相关配置信息
+             * @param ack   exchange 交换机是否成功接受到消息：true - 成功， false - 失败
+             * @param cause 失败原因
+             */
+            @Override
+            public void confirm(CorrelationData correlationData, boolean ack, String cause) {
+                System.out.println("confirm 方法被执行了...");
+                if (ack) {
+                    System.out.println("消息接受成功！" + cause);
+                } else {
+                    System.out.println("消息接受失败！" + cause);
+                }
+            }
+        });
+
+        template.convertAndSend(RabbitMqConfig.EXCHANGE_NAME + "111", "boot.start", "Hello Rabbit!");
     }
 
 }
